adcfab5f2fbd5a8ae712745541dbbca998addae5
ConstantKOUASSI12 <kouassiconstant94@gmail.com>
2026-05-03 17:32:27 +0200

    test: ajout des tests E2E Playwright addition

---
+++ app/e2e/calculator.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Calculatrice', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+  })
+
+  
+  test.describe('Addition', () => {
+    test('additionne 2 + 3 = 5', async ({ page }) => {
+      await page.click('button:has-text("2")')
+      await page.click('button:has-text("+")')
+      await page.click('button:has-text("3")')
+      await page.click('button:has-text("=")')
+      await expect(page.locator('.display-result')).toContainText('5')
+    })
+
+    test('additionne 0 + 0 = 0', async ({ page }) => {
+      await page.click('button:has-text("0")')
+      await page.click('button:has-text("+")')
+      await page.click('button:has-text("0")')
+      await page.click('button:has-text("=")')
+      await expect(page.locator('.display-result')).toContainText('0')
+    })
+  })
+})
