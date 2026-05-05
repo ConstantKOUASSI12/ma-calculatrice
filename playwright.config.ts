1c62fcf3e549ce877312d3c40676f782c0b909ca
ConstantKOUASSI12 <kouassiconstant94@gmail.com>
2026-05-05 19:46:11 +0200

    Mise a jour playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './app/e2e',
+  use: {
+    baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+  },
   webServer: {
     command: 'node .output/server/index.mjs',
     url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120_000,
   },
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
