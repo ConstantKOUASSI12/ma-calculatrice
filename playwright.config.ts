36677afbe1e0239c60716aa2030f8be531c88bb5
ConstantKOUASSI12 <kouassiconstant94@gmail.com>
2026-05-05 19:19:36 +0200

    Mise a jour ci.yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './app/e2e',
-    webServer: {
+  webServer: {
     command: 'node .output/server/index.mjs',
     url: 'http://localhost:3000',
   },
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 4 : undefined,
-  reuseExistingServer: !process.env.CI,
+  //reuseExistingServer: !process.env.CI,
   
 })
